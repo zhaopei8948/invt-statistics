975c10e89dc1dcf99d89daabd9c0570b93b9d4a1
zhaopei <zhaopei8948@hotmail.com>
2020-06-02 16:20:34 +0800

    添加所有状态统计功能

--- invt_statistics.py
+++ invt_statistics.py
@@ -131,9 +131,10 @@ def invtReleaseStatisticsByDate():
 
     now = datetime.now()
     beginTime = now + timedelta(days=-7)
+    conditionEndTime = None
 
     if oldBeginDate is None or oldEndDate is None:
-        return render_template('invt_statistics.html', invtIn=[], invtOut=[], beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'))
+        return render_template('invt_release_statistics.html', invtIn=[], invtOut=[], beginDate=now.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'))
 
     if oldBeginDate is not None:
         oldBeginDate = removeBlank(oldBeginDate)
@@ -148,32 +149,89 @@ def invtReleaseStatisticsByDate():
         beginTime = datetime.strptime(beginDate, '%Y%m%d')
         endTime = datetime.strptime(endDate, '%Y%m%d')
     except:
-        return render_template('invt_statistics.html', invtIn=[], invtOut=[], beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'), message='日期格式不对!')
+        return render_template('invt_release_statistics.html', invtIn=[], invtOut=[], beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'), message='日期格式不对!')
 
     if int(beginDate) > int(endDate):
         msg = '开始日期不能大于结束日期!'
-        return render_template('invt_statistics.html', invtIn=[], invtOut=[], beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'), message=msg)
+        return render_template('invt_release_statistics.html', invtIn=[], invtOut=[], beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'), message=msg)
 
+    conditionEndTime = endTime + timedelta(days=1)
     sql = '''
         select count(1) 总单量, sum((case when t.app_status = '800' then 1 else 0 end)) 外网放行单量,
-        round(sum((case when t.app_status = '800' then 1 else 0 end)) / count(1), 4) * 100 || '%' 外网放行比例,
-         sum((case when t.cus_status = '26' then 1 else 0 end)) 内网放行单量,
-         round(sum((case when t.cus_status = '26' then 1 else 0 end)) / count(1), 4) * 100 || '%' 内网放行比例
-         from ceb2_invt_head t
+        to_char(round(sum((case when t.app_status = '800' then 1 else 0 end)) / count(1), 4) * 100, 'fm990.00') || '%' 外网放行比例,
+        sum((case when t.cus_status = '26' then 1 else 0 end)) 内网放行单量,
+        to_char(round(sum((case when t.cus_status = '26' then 1 else 0 end)) / count(1), 4) * 100, 'fm990.00') || '%' 内网放行比例
+        from ceb2_invt_head t
         where t.sys_date >= to_date(:beginDate, 'yyyyMMdd')
         and t.sys_date < to_date(:endDate, 'yyyyMMdd')
     '''
-    invtInResult = executeSqlOne(sql, beginDate=beginDate, endDate=endDate)
+    invtInResult = executeSqlOne(sql, beginDate=beginDate, endDate=conditionEndTime.strftime('%Y%m%d'))
     sql = '''
        select count(1) 总单量, sum((case when t.app_status in ('800', '899') then 1 else 0 end)) 外网放行单量,
-        round(sum((case when t.app_status in ('800', '899') then 1 else 0 end)) / count(1), 4) * 100 || '%' 外网放行比例,
-         sum((case when t.cus_status in('26', '21') then 1 else 0 end)) 内网放行单量,
-         round(sum((case when t.cus_status in ('26', '21') then 1 else 0 end)) / count(1), 4) * 100 || '%' 内网放行比例
-         from ceb3_invt_head t
-        where t.sys_date >= to_date(:beginDate, 'yyyyMMdd')
-        and t.sys_date < to_date(:endDate, 'yyyyMMdd')
+       to_char(round(sum((case when t.app_status in ('800', '899') then 1 else 0 end)) / count(1), 4) * 100, 'fm990.00') || '%' 外网放行比例,
+       sum((case when t.cus_status in('26', '21') then 1 else 0 end)) 内网放行单量,
+       to_char(round(sum((case when t.cus_status in ('26', '21') then 1 else 0 end)) / count(1), 4) * 100, 'fm990.00') || '%' 内网放行比例
+       from ceb3_invt_head t
+       where t.sys_date >= to_date(:beginDate, 'yyyyMMdd')
+       and t.sys_date < to_date(:endDate, 'yyyyMMdd')
     '''
-    invtOutResult = executeSqlOne(sql, beginDate=beginDate, endDate=endDate)
+    invtOutResult = executeSqlOne(sql, beginDate=beginDate, endDate=conditionEndTime.strftime('%Y%m%d'))
     return render_template('invt_release_statistics.html', invtIn=invtInResult, invtOut=invtOutResult, beginDate=beginTime.strftime('%Y-%m-%d'), endDate=endTime.strftime('%Y-%m-%d'))
+
+@bp.route("/invtAllStatusStatisticsByDate")
+def invtAllStatusStatisticsByDate():
+    oldBeginDate = request.values.get('beginDate')
+    oldEndDate = request.values.get('endDate')
+
+    now = datetime.now()
+    beginTime = now + timedelta(days=-7)
+    conditionEndTime = None
+
+    if oldBeginDate is None or oldEndDate is None:
+        return render_template('invt_all_status_statistics.html', inAllStatus=[], inAllCusStatus=[], outAllStatus=[], outAllCusStatus=[],
+                               beginDate=now.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'))
+
+    if oldBeginDate is not None:
+        oldBeginDate = removeBlank(oldBeginDate)
+        beginDate = oldBeginDate.replace('-', '')
+    if oldEndDate is not None:
+        oldEndDate = removeBlank(oldEndDate)
+        endDate = oldEndDate.replace('-', '')
+
+    try:
+        logger.info('beginDate={}, oldBeginDate={}', beginDate, oldBeginDate)
+        logger.info('endDate={}, oldEndDate={}', endDate, oldEndDate)
+        beginTime = datetime.strptime(beginDate, '%Y%m%d')
+        endTime = datetime.strptime(endDate, '%Y%m%d')
+    except:
+        return render_template('invt_all_status_statistics.html', inAllStatus=[], inAllCusStatus=[], outAllStatus=[], outAllCusStatus=[],
+                               beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'), message='日期格式不对!')
+
+    if int(beginDate) > int(endDate):
+        msg = '开始日期不能大于结束日期!'
+        return render_template('invt_all_status_statistics.html', inAllStatus=[], inAllCusStatus=[], outAllStatus=[], outAllCusStatus=[],
+                               beginDate=beginTime.strftime('%Y-%m-%d'), endDate=now.strftime('%Y-%m-%d'), message=msg)
+
+    conditionEndTime = endTime + timedelta(days=1)
+    sql = '''
+    select tt0.app_status, tt0.qty, to_char(round(tt0.qty / tt1.total_qty, 4) * 100, 'fm990.00') || '%' from
+    (select
+     t.app_status, count(1) qty
+     from ceb2_invt_head t
+    where t.sys_date >= to_date(:beginDate, 'yyyy-MM-dd')
+    and t.sys_date < to_date(:endDate, 'yyyy-MM-dd')
+    group by t.app_status
+    order by count(1) desc
+    ) tt0, (select count(1) total_qty from ceb2_invt_head 
+    where sys_date >= to_date(:beginDate, 'yyyy-MM-dd') 
+    and sys_date < to_date(:endDate, 'yyyy-MM-dd')
+    ) tt1
+    '''
+    inAllStatus = executeSql(sql, beginDate=beginDate, endDate=conditionEndTime.strftime('%Y%m%d'))
+    inAllCusStatus = executeSql(sql.replace('app_status', 'cus_status'), beginDate=beginDate, endDate=conditionEndTime.strftime('%Y%m%d'))
+    outAllStatus = executeSql(sql.replace('ceb2', 'ceb3'), beginDate=beginDate, endDate=conditionEndTime.strftime('%Y%m%d'))
+    outAllCusStatus = executeSql(sql.replace('ceb2', 'ceb3').replace('app_status', 'cus_status'), beginDate=beginDate, endDate=conditionEndTime.strftime('%Y%m%d'))
+    return render_template('invt_all_status_statistics.html', inAllStatus=inAllStatus, inAllCusStatus=inAllCusStatus, outAllStatus=outAllStatus, outAllCusStatus=outAllCusStatus,
+                           beginDate=beginTime.strftime('%Y-%m-%d'), endDate=endTime.strftime('%Y-%m-%d'))
 
 app.register_blueprint(bp)
